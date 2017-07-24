d5ee00ad9daa6993d52032a7837b8d78b1a35596
James <JamesStumme@gmail.com>
2017-07-23 23:37:34 -0500

    Reformat feature flag service.

--- src/ngFeatureFlag/services/feature-flag.service.ts
+++ src/ngFeatureFlag/services/feature-flag.service.ts
@@ -8,58 +8,61 @@ import { Feature } from './feature';
 
 @Injectable()
 export class FeatureFlagService {
-  private get apiUri(): string {
-    return this.config.apiUri;
-  }
+    private get apiUri(): string {
+        return this.config.apiUri;
+    }
 
+    private get headers(): Headers {
+        const flagHeaders = new Headers();
+        flagHeaders.append('X-Tenant', this.config.tenantId);
+        flagHeaders.append('X-Application', this.config.applicationId);
+        return flagHeaders;
+    }
 
+    constructor(private config: FeatureFlagConfig, private http: Http) {}
 
-  private get headers(): Headers {
-    const flagHeaders = new Headers();
-    flagHeaders.append('X-Tenant', this.config.tenantId);
-    flagHeaders.append('X-Application', this.config.applicationId);
-    return flagHeaders;
-  }
-
-  constructor(private config: FeatureFlagConfig, private http: Http) { }
-
-  public isEnabled(featureName: string): Observable<boolean | Boolean> {
-    return this.GetFeatureStatus(featureName)
-      .catch((res: Response) => {
-        if (res.status !== 404) {
-          console.error('Failed to get feature flags: ' + res.text());
-        }
-        return Observable.of(false);
-      });
-  }
-
-  public isDisabled(featureName: string): Observable<boolean> {
-    return this.GetFeatureStatus(featureName)
-      .map((status: boolean) => !status)
-      .catch((res: Response) => {
-        if (res.status !== 404) {
-          console.error('Failed to get feature flags: ' + res.text());
-        }
-        return Observable.of(false);
-      });
-  }
-
-  private GetFeatureStatus(featureName: string): Observable<Boolean> {
-    return this.http.get(this.apiUri + 'feature/' + featureName, { headers: this.headers })
-      .map((res: Response) => {
-        const feature = <Feature>res.json();
-        return feature.isEnabled;
-      });
-  }
-
-  private GetAllFeatures(): Observable<Map<string, boolean>> {
-    return this.http.get(this.apiUri + 'feature' , {headers: this.headers})
-      .map((res: Response) => {
-        const features = new Map<string, boolean>();
-        <Feature[]>(res.json()['Features']).forEach((feature: Feature) => {
-          features[feature.name] = feature.isEnabled;
+    public isEnabled(featureName: string): Observable<boolean | Boolean> {
+        return this.GetFeatureStatus(featureName).catch((res: Response) => {
+            if (res.status !== 404) {
+                console.error('Failed to get feature flags: ' + res.text());
+            }
+            return Observable.of(false);
         });
-        return features;
-      });
-  }
+    }
+
+    public isDisabled(featureName: string): Observable<boolean> {
+        return this.GetFeatureStatus(featureName)
+            .map((status: boolean) => !status)
+            .catch((res: Response) => {
+                if (res.status !== 404) {
+                    console.error('Failed to get feature flags: ' + res.text());
+                }
+                return Observable.of(false);
+            });
+    }
+
+    private GetFeatureStatus(featureName: string): Observable<Boolean> {
+        return this.http
+            .get(this.apiUri + 'feature/' + featureName, {
+                headers: this.headers
+            })
+            .map((res: Response) => {
+                const feature = <Feature>res.json();
+                return feature.isEnabled;
+            });
+    }
+
+    private GetAllFeatures(): Observable<Map<string, boolean>> {
+        return this.http
+            .get(this.apiUri + 'feature', { headers: this.headers })
+            .map((res: Response) => {
+                const features = new Map<string, boolean>();
+                <Feature[]>res
+                    .json()['Features']
+                    .forEach((feature: Feature) => {
+                        features[feature.name] = feature.isEnabled;
+                    });
+                return features;
+            });
+    }
 }

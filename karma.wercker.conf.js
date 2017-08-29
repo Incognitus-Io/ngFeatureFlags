5a3828fce2231cf6e1235aa2f20897764d03b3c6
James <JamesStumme@gmail.com>
2017-08-28 22:59:23 -0500

    Fix code coverage to use ng cli.

--- karma.wercker.conf.js
+++ karma.wercker.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
+      reports: ['json'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
